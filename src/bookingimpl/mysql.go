494b9c4ae56e1893ea890dc18cb66625583e0d5b
nkulkarni10 <nikhil.kulkarni10@gmail.com>
2021-09-15 19:03:31 +0530

    Added interfaces implementation

--- src/bookingimpl/mysql.go
+++ src/bookingimpl/mysql.go
@@ -14,39 +14,44 @@ var db *gorm.DB
 
 var err error
 
+type dao struct{}
+
+func NewBookingDao() domain.BookingDao {
+	return &dao{}
+}
 func init() {
 	db, err = gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/booking_api?charset=utf8&parseTime=True")
 	db.AutoMigrate(&domain.Booking{})
 }
 
-func CreateBooking(booking *domain.Booking) error{
+func (*dao) CreateBooking(booking *domain.Booking) error {
 	err := db.Create(booking).Error
 	return err
 }
 
-func ReturnAllBookings() []*domain.Booking {
+func (*dao) ReturnAllBookings() []*domain.Booking {
 	bookings := make([]*domain.Booking, 0)
 	db.Find(&bookings)
 	return bookings
 }
 
-func ReturnSingleBooking(id string) (*domain.Booking, error){
+func (*dao) ReturnSingleBooking(id string) (*domain.Booking, error) {
 	ID, err := strconv.Atoi(id)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
 	booking := &domain.Booking{
 		Id: ID,
 	}
 	err = db.First(booking).Error
-	if gorm.IsRecordNotFoundError(err){
+	if gorm.IsRecordNotFoundError(err) {
 		err = domain.ErrNotFound
 		return nil, err
 	}
 	return booking, nil
 }
 
-func UpdateBooking(booking *domain.Booking) error{
+func (*dao) UpdateBooking(booking *domain.Booking) error {
 	//db.Exec("UPDATE Bookings SET User =? WHERE Id=?", booking.User, booking.Id)
 	err := db.Model(booking).Update(booking).Error
 	if err != nil {
@@ -56,14 +61,14 @@ func UpdateBooking(booking *domain.Booking) error{
 	return nil
 }
 
-func DeleteBooking(id string) error{
+func (*dao) DeleteBooking(id string) error {
 	//db.Exec("DELETE FROM Bookings WHERE Id=?", id)
 	ID, err := strconv.Atoi(id)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	r := db.Delete(domain.Booking{Id: ID})
-	if r.RowsAffected == 0{
+	if r.RowsAffected == 0 {
 		return domain.ErrNotFound
 	}
 	return nil
