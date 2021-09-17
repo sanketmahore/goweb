e2bc22b779f1d7e6bd1228c896c27241277bab08
sanketmahore <39237534+sanketmahore@users.noreply.github.com>
2021-09-17 13:10:12 +0530

    Testcase for Create()

--- src/bookingimpl/service_test.go
+++ src/bookingimpl/service_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestListBooking(t *testing.T) {
@@ -31,4 +32,35 @@ func TestListBooking(t *testing.T) {
 		)
 	}
 
+}
+
+func TestCreateBooking(t *testing.T) {
+	dao := &mocks.BookingDao{}
+	service := NewBookingService(dao)
+	cases := []struct {
+		name       string
+		bookingReq *domain.Booking
+		err        error
+	}{
+
+		{
+			"success",
+			&domain.Booking{Id: 1, User: "mock", Members: 3},
+			nil,
+		},
+		{
+			"duplicate",
+			&domain.Booking{Id: 1, User: "mock", Members: 3},
+			domain.ErrConflict,
+		},
+	}
+	for _, c := range cases {
+		t.Run(
+			c.name, func(t *testing.T) {
+				dao.On("CreateBooking", mock.Anything).Return(c.err).Once()
+				res := service.Create(c.bookingReq)
+				assert.Equal(t, c.err, res)
+			},
+		)
+	}
 }
